07ac9714fb617785305a043ac910ccd10cfe95b6
Ana Júlia Paifer <132628720+paiferana@users.noreply.github.com>
2024-10-15 22:45:25 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Register from './components/Register';
+import PhotoGallery from './components/PhotoGallery';
+
+function App() {
+  return (
+    <>
+    <Header></Header>
+    <Home></Home>
+    <About></About>
+    <Register></Register>
+    <PhotoGallery></PhotoGallery>
+    </>
+  );
+}
+
+export default App;
